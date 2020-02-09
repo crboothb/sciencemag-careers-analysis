1fb307d812e4c3e9c432481854a14e7266f20c10
crboothb <crboothb@indiana.edu>
2020-02-08 20:25:10 -0500

    creating way to make stable sample for hand-coding

---
+++ scripts/sample.py
@@ -0,0 +1,50 @@
+# Generate stable samples for code_interface
+
+import random
+import pickle
+import pandas as pd
+
+import import_func as imp
+
+
+
+full_filename = "../data/by_article_fulltext_112919-2.jl"
+
+# get full text dataset as a df
+# full_df = imp.init_df(full_filename, "full")
+
+# get full dataset as a dict
+# full_dict = imp.init_df(full_filename, "full", "dict") # Apparently this still returns a pandas dataframe... Awkward
+full_dict = imp.init_df(full_filename, "full", "df") # This variable is still saved as full_dict so that I don't have to change the rest of the variables
+# print(full_dict.keys())
+# print(full_df.head())
+
+# print(full_dict.head())
+
+# for key in full_dict.keys():
+#     print(len(full_dict[key]))
+
+def drop_short(df):
+    to_drop = []
+    for i in df.index.values:
+        # print(i)
+        text = df.loc[i, "text"]
+        if len(text.split(" ")) < 200:
+            # print(i)
+            to_drop.append(i)
+
+    # print(to_drop)
+    return(df.drop(to_drop))
+
+full_dict = drop_short(full_dict)
+
+
+# print("###########################")
+# for key in full_dict.keys():
+#     print(len(full_dict[key]))
+
+print("data loaded")
+
+
+sample200 = random.sample(list(full_dict.index.values), 200)
+sample200_chunks = []
