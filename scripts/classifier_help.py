bc31b37904871f5282296d744558ad6d2e73012b
crboothb <crboothb@indiana.edu>
2020-02-16 22:19:12 -0500

    classifier functions

--- scripts/classifier_help.py
+++ scripts/classifier_help.py
@@ -1,5 +1,6 @@
 import string
 import re
+import pandas as pd
 
 def no_punctuation(text, quotes=False):
     for mark in string.punctuation:
@@ -10,7 +11,7 @@ def no_punctuation(text, quotes=False):
     return(text)
 
 def replace_quotes(text):
-    text = text.replace("\“","\"")
+    # text = text.replace("\“","\"")
 
     quotes = re.findall(r'\"(.+?)\"', text)
 
@@ -21,3 +22,51 @@ def replace_quotes(text):
         # print(quote+"\n")
     return(text)
 
+def count_pro(clean_text, person):
+    if person == "first":
+        pronouns = [" i "," im ", " ive ", " id "," my ", " me ", " myself "]
+    elif person == "second":
+        pronouns = [" you "," youre ", " youve "," youd "," your ", " yourself "]
+    else:
+        pronouns = []
+    
+    count = 0
+
+    clean_text = replace_quotes(clean_text)
+
+    for pro in pronouns:
+        count += clean_text.count(pro)
+    return(count)
+
+
+def pronouns(f_df, sample="none"):
+    first_pronouns = [" i "," im ", " ive ", " id "," my ", " me ", " myself "]
+    second_pronouns = [" you "," youre ", " youve "," youd "," your ", " yourself "]
+    third_pronouns = []
+
+    if sample == "none":
+        sample = [i for i in range(len(f_df))]
+
+    counts = {}
+    counts4df = {"id":[],"year":[],"first":[],"second":[],"wc":[]}
+
+    for samp in sample:
+        count1 = 0
+        count2 = 0
+        w_text = no_punctuation(f_df["text"][samp], quotes=True)
+        w_text = replace_quotes(w_text)
+        w_text = no_punctuation(w_text, quotes=False)
+        for pro in first_pronouns:
+            count1 += w_text.count(pro)
+        for pro in second_pronouns:
+            count2 += w_text.count(pro)
+        wc = len(w_text.split(" "))
+        counts[samp] = {"first":count1, "second":count2, "wc":wc}
+        counts4df["id"].append(samp)
+        counts4df["year"].append(f_df["year"][samp])
+        counts4df["first"].append(count1)
+        counts4df["second"].append(count2)
+        counts4df["wc"].append(wc)
+
+    c_df = pd.DataFrame(counts4df)
+    return(c_df)
