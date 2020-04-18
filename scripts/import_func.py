2ec6212b9406d6ba7aed7ef798b49667d5f3f125
crboothb <crboothb@indiana.edu>
2020-04-17 21:06:39 -0600

    line breaks out

--- scripts/import_func.py
+++ scripts/import_func.py
@@ -222,7 +222,7 @@ def process(list, focus, out_form, genre = "none"):
                         break
                     if record == True:
                         f_text_list.append(string.strip().replace("\n", ""))
-                f_text = "\n".join(f_text_list)
+                f_text = "".join(f_text_list)
                 # print(w_text)
                 # print("#######################################")
                 # last 16 lines are junk
