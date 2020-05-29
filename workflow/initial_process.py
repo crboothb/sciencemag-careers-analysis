b6131f55a473b580b7f62e9a4e5901cdbdad76c1
crboothb <crboothb@indiana.edu>
2020-05-29 14:01:07 -0600

    remove unnecessary lines

--- workflow/initial_process.py
+++ workflow/initial_process.py
@@ -1,22 +1,8 @@
 import pandas as pd
-import ast
-import re
-import numpy as np
 import sys
 
 sys.path.append('../scripts/')
 import import_func as imp
-# import tags_work as tgs
-# import desc_vis as vis
-
-# something a warning told me to do?
-# from pandas.plotting import register_matplotlib_converters
-# register_matplotlib_converters()
-
-# pd.set_option('display.max_rows', 500)
-# pd.set_option('display.max_columns', 500)
-# pd.set_option('display.max_colwidth', 100)
-# pd.set_option('display.width', 150)
 
 infile = "../data/by_article_fulltext_020920.jl"
 outfile = "../data/initial_processed.csv"
@@ -44,8 +30,6 @@ full_df = full_df.drop(
 
 
 print("data process done")
-
-# print([col for col in full_df.columns])
 
 full_df.to_csv(outfile, index=False)
 
